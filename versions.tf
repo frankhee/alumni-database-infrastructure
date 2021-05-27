80355a66c67e4cb5c69d6c41a4b1003cd547aa9b
Yangrongrui He <frankhe@yangrongruismbp.home>
2021-05-27 18:42:40 -0400

    Added tfvars file into gitignore

--- versions.tf
+++ versions.tf
@@ -1,9 +1,10 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.42.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.20.0"
     }
   }
+
   required_version = "~> 0.14"
 }
